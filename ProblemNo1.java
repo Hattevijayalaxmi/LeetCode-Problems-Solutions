8129e33834912c06e79c8ce92de93108abef82bb
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-05-21 14:18:19 +0530

    Update ProblemNo1.java

--- ProblemNo1.java
+++ ProblemNo1.java
@@ -12,3 +12,21 @@ class Solution {
         return new int[] {};
     }
 }
+
+
+//Using HashMap
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+            
+            HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+            for(int i = 0; i  < nums.length; i++) {
+                if (map.containsKey(nums[i])) {
+                    return new int[] { map.get(nums[i]), i};
+                }else {
+                  map.put(target - nums[i], i);
+                }
+            }
+             return new int[] {};
+    }
+}
