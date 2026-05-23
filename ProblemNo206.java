b7b033d987cb1e3fe0034313369e0c918be968fd
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-05-23 13:50:10 +0530

    Create ProblemNo206.java

---
+++ ProblemNo206.java
@@ -0,0 +1,15 @@
+// Reverse Linked List - LeetCode 206
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while(head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+}
