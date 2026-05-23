77f7b4816330773e44d61478bbec39f4cfc21cdd
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-05-23 14:08:04 +0530

    Create ProblemNo876.java

---
+++ ProblemNo876.java
@@ -0,0 +1,15 @@
+// Middle of the Linked List - LeetCode 876
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
