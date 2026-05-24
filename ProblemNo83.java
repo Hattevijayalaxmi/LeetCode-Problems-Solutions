48c3d4b49ebea0c5c1a1bd9733ff52da37978e64
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-05-24 12:59:29 +0530

    Create ProblemNo83.java

---
+++ ProblemNo83.java
@@ -0,0 +1,16 @@
+// Remove Duplicates From Sorted List - LeetCode 83
+
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode temp = head;
+        while(temp != null && temp.next != null) {
+            if (temp.val == temp.next.val) {
+                temp.next = temp.next.next;
+            }else {
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
