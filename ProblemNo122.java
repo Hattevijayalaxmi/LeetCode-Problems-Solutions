0e9b21d2c4644f83b50a8e460658c6c09705ab7b
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-05-25 13:49:21 +0530

    Create ProblemNo122.java

---
+++ ProblemNo122.java
@@ -0,0 +1,17 @@
+// Best Time to Buy and Sell Stock 2 - LeetCode 122
+
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+}
