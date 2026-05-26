059b85c0fe8d3b9a2c88cc822f4543954ea814dd
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-05-26 13:24:27 +0530

    Create ProblemNo136.java

---
+++ ProblemNo136.java
@@ -0,0 +1,14 @@
+// Single Number - LeetCode 136
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+        int result = 0;
+
+        for(int num : nums) {
+            result ^= num;
+        }
+
+        return result;
+    }
+}
