67d5c8975f91a436d2406d6773e9905a5fe5061c
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-05-26 12:52:51 +0530

    Create ProblemNo145.java

---
+++ ProblemNo145.java
@@ -0,0 +1,21 @@
+// Binary Tree Postorder Traversal - LeetCode 145
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        
+        List<Integer> result = new ArrayList<>();
+
+        postorder(root, result);
+
+        return result;
+    }
+
+    public void postorder(TreeNode node, List<Integer> result) {
+
+        if(node == null)
+            return;
+        postorder(node.left, result);
+        postorder(node.right, result);
+        result.add(node.val);
+    }
+}
