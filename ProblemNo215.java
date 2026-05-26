734c326f0b603cbe8b147e001dc9c855e43d7047
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-05-26 13:12:57 +0530

    Create ProblemNo215.java

---
+++ ProblemNo215.java
@@ -0,0 +1,16 @@
+// Kth Largest Element in an Array
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for(int num : nums) {
+            minHeap.offer(num);
+            if(minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        return minHeap.peek();
+    }
+}
