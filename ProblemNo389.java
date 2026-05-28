3b6e3d266813df43ce1d5a1aa97b0f4e25250464
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-05-28 13:33:42 +0530

    Create ProblemNo389.java

---
+++ ProblemNo389.java
@@ -0,0 +1,18 @@
+// Find the Difference - LeetCode 389
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+
+        int xor = 0;
+
+        for(char c : s.toCharArray()) {
+            xor ^= c;
+        }
+
+        for(char c : t.toCharArray()) {
+            xor ^= c;
+        }
+
+        return (char) xor;
+    }
+}
