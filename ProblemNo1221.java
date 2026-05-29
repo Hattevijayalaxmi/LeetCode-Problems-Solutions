ebd75c9d228f7db55e32b30b2e40cb3595c860b0
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-05-29 20:37:06 +0530

    Create ProblemNo1221.java

---
+++ ProblemNo1221.java
@@ -0,0 +1,23 @@
+// Split a String in Balanced Strings - LeetCode 1221
+
+class Solution {
+    public int balancedStringSplit(String s) {
+
+        int balance = 0;
+        int count = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+
+            if(s.charAt(i) == 'L') {
+                balance++;
+            } else {
+                balance--;
+            }
+            if(balance == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
