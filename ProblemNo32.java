d1cba236195b66cd9adb48272f1688b49c54fa76
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-06-01 21:00:37 +0530

    Create ProblemNo32.java

---
+++ ProblemNo32.java
@@ -0,0 +1,28 @@
+// Longest Vaild Parentheses - LeetCode 32
+
+import java.util.*;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+
+        int n = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    n = Math.max(n, i - stack.peek());
+                }
+            }
+        }
+
+        return n;
+    }
+}
