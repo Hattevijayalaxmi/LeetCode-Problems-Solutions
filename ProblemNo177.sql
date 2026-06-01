53c2672f47e23da350ca773ccc3e55f8100508f7
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-06-01 21:07:12 +0530

    Create ProblemNo177.sql

---
+++ ProblemNo177.sql
@@ -0,0 +1,13 @@
+-- Nth Highest Salary - LeetCode 177
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    SET N = N - 1;
+
+    RETURN (
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET N
+    );
+END
