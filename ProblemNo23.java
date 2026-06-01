e9ed6dd86f6643ccaf018a6cab887b7bec2bb8ab
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-06-01 20:57:06 +0530

    Create ProblemNo23.java

---
+++ ProblemNo23.java
@@ -0,0 +1,32 @@
+// Merge k Sorted Lists - LeetCode 23
+
+import java.util.PriorityQueue;
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(
+            (a, b) -> a.val - b.val
+        );
+
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.offer(node);
+            }
+        }
+
+        ListNode n = new ListNode(0);
+        ListNode curr = n;
+
+        while (!pq.isEmpty()) {
+            ListNode node = pq.poll();
+            curr.next = node;
+            curr = curr.next;
+
+            if (node.next != null) {
+                pq.offer(node.next);
+            }
+        }
+
+        return n.next;
+    }
+}
