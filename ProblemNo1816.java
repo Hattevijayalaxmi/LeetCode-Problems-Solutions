ad8bbf1c816faa8972ca5cbb658d761c07d073ee
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-06-02 21:58:56 +0530

    Create ProblemNo1816.java

---
+++ ProblemNo1816.java
@@ -0,0 +1,18 @@
+// Truncate Sentence - LeetCode 1816
+
+class Solution {
+    public String truncateSentence(String s, int k) {
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                count++;
+                if (count == k) {
+                    return s.substring(0, i);
+                }
+            }
+        }
+
+        return s;
+    }
+}
