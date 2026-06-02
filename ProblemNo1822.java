b7bdbe85d62ac3515d1e3d853e7cc90a75efa154
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-06-02 22:00:12 +0530

    Create ProblemNo1822.java

---
+++ ProblemNo1822.java
@@ -0,0 +1,19 @@
+// Sign of the product of an Array - LeetCode 1822
+
+class Solution {
+    public int arraySign(int[] nums) {
+        int sign = 1;
+
+        for (int num : nums) {
+            if (num == 0) {
+                return 0;
+            }
+
+            if (num < 0) {
+                sign *= -1;
+            }
+        }
+
+        return sign;
+    }
+}
