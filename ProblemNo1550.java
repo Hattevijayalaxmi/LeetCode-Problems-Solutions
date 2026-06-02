57e9105101c6df7d58d2ba929bbb6e02647ca888
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-06-02 21:47:14 +0530

    Create ProblemNo1550.java

---
+++ ProblemNo1550.java
@@ -0,0 +1,20 @@
+// Three Consecutive Odds - LeetCode 1550
+
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+
+        for (int num : arr) {
+            if (num % 2 != 0) {
+                count++;
+                if (count == 3) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+
+        return false;
+    }
+}
