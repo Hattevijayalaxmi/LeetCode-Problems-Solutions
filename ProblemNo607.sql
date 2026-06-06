1233b6581549e05a475fa1d3015c7a8d04949f1c
Vijayalaxmi Hatte <hattevijayalaxmi@gmail.com>
2026-06-06 13:00:24 +0530

    Create ProblemNo607.sql

---
+++ ProblemNo607.sql
@@ -0,0 +1,11 @@
+-- Sales Person - LeetCode 607
+
+SELECT name
+FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT o.sales_id
+    FROM Orders o
+    JOIN Company c
+        ON o.com_id = c.com_id
+    WHERE c.name = 'RED'
+);
